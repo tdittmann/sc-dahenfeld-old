e86fc186d8f1fcb65cbcf7d659ec8ccd52ca2b87
Timo Dittmann <timo.dittmann@kaufland.com>
2017-06-21 00:22:22 +0200

    #1 New images for tour

--- src/pages/tour/tour.component.ts
+++ src/pages/tour/tour.component.ts
@@ -16,15 +16,20 @@ export class TourComponent {
       image: "assets/img/logo.png",
     },
     {
-      title: "Spielplan, Tabelle und Kader",
-      description: "Für die <b>1. und 2. Mannschaft</b> ist der gesamte Spielplan auf Deinem Smartphone! Verpasse kein Spiel und erhalte zusätzlich Informationen zu jedem Spieler. Mit der <b>Tabelle</b> hast du den aktuellen Stand der beiden Mannschaften im Blick.",
-      image: "assets/img/tour/tabelleSpielplan.png",
+      title: "Aktuelle Neuigkeiten",
+      description: "Bleib am Ball und verpasse keine Neuigkeit des SCD. Egal ob Verein, Fußball, Turnen oder Tischtennis. Mit der SC Dahenfeld App verpasst Du nichts!",
+      image: "assets/img/tour/news.png",
     },
     {
-      title: "Vereinskalender",
-      description: "Verpasse kein Spiel des SCD: mit dem Vereinskalender werden <b>sämtliche Spiele (inkl. Jugend) in den kommenden 2 Wochen</b> angezeigt.",
-      image: "assets/img/tour/vereinskalender.png",
+      title: "Teaminformationen",
+      description: "Für <b>sämtliche Mannschaften</b> des SC Dahenfeld ist der gesamte Spielplan auf Deinem Smartphone! Verpasse kein Spiel und erhalte zusätzlich Informationen zu jedem Spieler. Mit der <b>Tabelle</b> hast du den aktuellen Stand der Mannschaften im Blick.",
+      image: "assets/img/tour/teamDetails.png",
     },
+    // {
+    //   title: "Vereinskalender",
+    //   description: "Verpasse kein Spiel des SCD: mit dem Vereinskalender werden <b>sämtliche Spiele (inkl. Jugend) in den kommenden 2 Wochen</b> angezeigt.",
+    //   image: "assets/img/tour/vereinskalender.png",
+    // },
     {
       title: "Turnen & Tischtennis",
       description: "Das Angebot der Turn- und Tischtennisabteilung kannst Du jederzeit über die App abrufen. Neben <b>Trainingszeiten</b> findest Du auch <b>Ansprechpartner</b> für die jeweiligen Angebote!",
